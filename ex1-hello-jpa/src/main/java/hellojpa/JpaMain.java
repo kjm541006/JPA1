2957268bd6862d1af73e2dc465656a79cc142b0d
kjm541006 <kjm541113@gmail.com>
2025-01-23 16:31:49 +0900

    영속 컨텍스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,6 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.List;
 
 public class JpaMain {
 
@@ -15,11 +14,21 @@ public class JpaMain {
         tx.begin();
 
         try{
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, 100L);
+            // 커밋 전 조회 가능
+            System.out.println("findMember.Id = " + findMember.getId());
+            System.out.println("findMember.Name = " + findMember.getName());
+            
+            Member findMember2 = em.find(Member.class, 100L);
+            // 영속 엔티티의 동일성 보장
+            boolean checkSameEntity = findMember == findMember2;
+            System.out.println("checkSameEntity = " + checkSameEntity);
 
             tx.commit();
         } catch(Exception e){
