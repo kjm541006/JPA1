9b70a1291739e250bbc48100efe92c1b2ed4673f
kjm541006 <kjm541113@gmail.com>
2025-02-04 15:05:42 +0900

    JPA 영속성 컨텍스트 초기화

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,23 @@ public class JpaMain {
         tx.begin();
 
         try{
+            // 1차 캐시에 없으면 db에서 조회함
             Member member = em.find(Member.class, 160L);
             member.setName("AAA");
 
+            System.out.println("===============");
+
+            // select문 실행 x , 영속성 컨텍스트에서 조회
+            Member member2 = em.find(Member.class, 160L);
+            member2.setName("BBB");
+
+            System.out.println("===============");
+
+            // 영속성 컨텍스트 완전 초기화
             em.clear();
 
-            Member member2 = em.find(Member.class, 160L);
+            // select문 실행, db에서 조회
+            Member member3 = em.find(Member.class, 160L);
 
             System.out.println("===============");
             tx.commit();
