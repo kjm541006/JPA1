5a892c93ce341a2da913f89a776da3b362597b21
kjm541006 <kjm541113@gmail.com>
2025-02-12 16:53:00 +0900

    엔티티 매핑 세팅

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    private Long orderId;
+
+    private Long itemId;
+
+    private int orderPrice;
+
+    private int count;
+}
